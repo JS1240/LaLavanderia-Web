81d5fc0c51a89669587ef233e1f618cc3ad453ef
Jure Sunic <juresunic@hotmail.com>
2022-07-14 21:20:59 +0200

    Added some more style to the page

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Nav, NavbarContainer, NavLogo} from './NavbarElements';
+import { FaBars } from 'react-icons/fa';
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
 
 const Navbar = () => {
   return (
@@ -7,6 +8,14 @@ const Navbar = () => {
         <Nav>
             <NavbarContainer>
                 <NavLogo to='/'>LaLavanderia</NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to='onama'>O Nama</NavLinks>
+                    </NavItem>
+                </NavMenu>
             </NavbarContainer>
         </Nav>
     </>  
